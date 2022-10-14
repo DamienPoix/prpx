9166042b3e37130d4c07ac1180db7863b814de5b
Damien poix <39798430+DamienPoix@users.noreply.github.com>
2022-10-14 16:12:42 +0200

    fondation footer réaliser

--- views/footer.php
+++ views/footer.php
@@ -1,8 +1,34 @@
-            <div id="footer">
-                <div id="footer-copyright">
-                    <p class="center" style="color:white"><i class="fa-regular fa-copyright"></i>POSER ROBOT PX</p>
+            <footer class="text-center">
+                <div class="container-fluid text-center border bg-light">
+                    <div class="row mt-3">
+                        <div class="col-3">
+                            <h3 class="fw-semibold tex-uppercase moderno">pose robot px</h3>
+                            <p>POSE ROBOT PX est une entreprise de pose de robot de tonte.Fondé en 2022 nous agissons principalement dans les hauts-de-france. </p>
+                        </div>
+                        <div class="col-2">
+                            <h3 class="fw-semibold tex-uppercase moderno">utilitaire 1</h3>
+                            <p><a href="#!" class="text-reset">Lien 1</a></p>
+                            <p><a href="#!" class="text-reset">Lien 2</a></p>
+                            <p><a href="#!" class="text-reset">Lien 3</a></p>
+                        </div>
+                        <div class="col-3">
+                            <h3 class="fw-semibold tex-uppercase moderno">utilitaire 2</h3>
+                            <p><a href="#!" class="text-reset">Lien 1</a></p>
+                            <p><a href="#!" class="text-reset">Lien 2</a></p>
+                            <p><a href="#!" class="text-reset">Lien 3</a></p>
+                        </div>
+                        <div class="col-4">
+                            <h3 class="fw-semibold tex-uppercase moderno">contact</h3>
+                            <p><i class="fa-solid fa-phone me-3"></i>06 33 06 29 01</p>
+                            <p><i class="fa-solid fa-envelope me-3"></i>poserobotpx@gmail.com</p>
+                            <p><i class="fa-brands fa-facebook-f me-3"></i><a href="https://www.facebook.com/PoseRobotPX" class="text-reset">PoseRobotPX</a></p>
+                        </div>
+                    </div>
+                    <div class="row border-top" style="background-color: rgba(0, 0, 0, 0.05)">
+                        <p class="col-12 black mt-3"><i class="fa-regular fa-copyright me-1 cr-footer"></i>POSE ROBOT PX</p>
+                    </div>
                 </div>
-            </div>
+            </footer>
         </div>
     </body>
 </html>
--- views/header.php
+++ views/header.php
@@ -2,11 +2,14 @@
 <!DOCTYPE html>
 <html> 
   <head>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="icon" type="icon" href="assets/img/prpx.png" />
+    <link href="https://fonts.googleapis.com/css2?family=MuseoModerno&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <script src="https://kit.fontawesome.com/5f0f3c52b6.js" crossorigin="anonymous"></script>
     <title>PRPX</title> 
   </head>
   <body>
-    <div class="container"> 
+    <div class="container-fluid"> 
