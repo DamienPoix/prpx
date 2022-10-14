537ab52d7bee84f5898718dd30e692b5732a20f8
Damien poix <39798430+DamienPoix@users.noreply.github.com>
2022-10-14 16:45:32 +0200

    mise au propre

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php include'views/header.php'; ?>
     <h1 class="row">POSE ROBOT PX</h1> 
     <p class="row">je suis un <?php echo $banana ?> réussi</p>  
-
-
 <?php include'views/footer.php'?>
--- views/header.php
+++ views/header.php
@@ -6,8 +6,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="icon" type="icon" href="assets/img/prpx.png" />
+    <!-- font moderno -->
     <link href="https://fonts.googleapis.com/css2?family=MuseoModerno&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
+    <!-- fontawesome (icon) -->
     <script src="https://kit.fontawesome.com/5f0f3c52b6.js" crossorigin="anonymous"></script>
     <title>PRPX</title> 
   </head>
