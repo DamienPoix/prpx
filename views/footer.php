54a03c9dc1cf3c5f0f0f7a7760c972cc53130e21
Damien poix <39798430+DamienPoix@users.noreply.github.com>
2022-10-14 14:02:04 +0200

    footer+début css+icon

--- views/footer.php
+++ views/footer.php
@@ -1,2 +1,8 @@
-</body>
+            <div id="footer">
+                <div id="footer-copyright">
+                    <p class="center" style="color:white"><i class="fa-regular fa-copyright"></i>POSER ROBOT PX</p>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
--- views/header.php
+++ views/header.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet" href="assets/css/style.css" />
     <link rel="icon" type="icon" href="assets/img/prpx.png" />
     <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/5f0f3c52b6.js" crossorigin="anonymous"></script>
     <title>PRPX</title> 
   </head>
-  <body> 
+  <body>
+    <div class="container"> 
