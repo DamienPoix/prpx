72106408f8b19ae55e762f44b687fc915186cb51
Damien poix <39798430+DamienPoix@users.noreply.github.com>
2022-10-17 13:29:49 +0200

    card + fix taille img

--- index.php
+++ index.php
@@ -3,36 +3,33 @@ include 'classes/path.php';
 include_once path::getViewsPath().'header.php';
 ?>
     <h1 class="row">POSE ROBOT PX</h1> 
-
-    <div class="card mb-3">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="assets/img/test-robots.png" class="img-fluid rounded-start" alt="robot tondeuse">
-    </div>
-    <div class="col-md-8 lightgreen">
-      <div class="card-body">
-        <h3 class="card-title">Présentation</h3>
-        <p class="card-text">texte de présentation de ce que l'on propose</p>
-        <p class="card-text"><small class="text-muted">lien vers un bouton pour plus d'info?</small></p>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="card mb-3">
-  <div class="row g-1">
-      <div class="col-md-8 ">
-          <div class="card-body">
-              <h3 class="card-title">Hivernage et maintenance</h3>
-              <p class="card-text">texte de présentation de ce que l'on propose</p>
-              <p class="card-text"><small class="text-muted">lien vers un bouton pour plus d'info?</small></p>
+    
+    <div class="container mt-5">
+         <div class="card mt-2 mb-2">
+            <div class="row ">
+                <div class="col-4">
+                    <img src="assets/img/test-robots.png" alt="machine nettoyage à froid" height="250" width="350" class="rounded-start"/>
+                </div>
+                <div class="col-8 card-body">
+                    <h3 class="card-title">Hivernage et maintenance</h3>
+                    <p class="card-text">texte de présentation de ce que l'on propose</p>
+                    <p class="card-text"><small class="text-muted">lien vers un bouton pour plus d'info?</small></p>
+                </div>
+            </div>
+        </div>   
+        <div class="card mt-2 mb-2">
+            <div class="row g-0">
+                <div class="col-4">
+                    <img src="assets/img/nettoyage.jpg" alt="machine nettoyage à froid" height="250" width="350" class="rounded-start"/>
+                </div>
+                <div class="col-8 card-body">
+                    <h3 class="card-title">Hivernage et maintenance</h3>
+                    <p class="card-text">texte de présentation de ce que l'on propose</p>
+                    <p class="card-text"><small class="text-muted">lien vers un bouton pour plus d'info?</small></p>
+                </div>
             </div>
         </div>
-        <div class="col-md-4">
-          <img src="assets/img/nettoyage.jpg" class="img-fluid rounded-end" alt="machine à froid">
-        </div>
-  </div>
-</div>
+    </div>
 
 
 <?php
