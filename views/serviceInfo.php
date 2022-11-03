2895c9d6c0e566341568629890d9358bde91e3f2
Damien poix <39798430+DamienPoix@users.noreply.github.com>
2022-11-03 15:19:52 +0100

    ajout de contenu

--- views/serviceInfo.php
+++ views/serviceInfo.php
@@ -4,19 +4,31 @@ include_once 'header.php';
 ?>
 <h1 class="moderno text-success text-center title">Installation et services</h1>
 <h2 class="moderno text-success">Installation d'un robot étape par étape.</h2>
-<h3>1.Prise de rendez-vous pour un premier rdv</h3>
 <div class="moderno">
+<h3>1.Prise de rendez-vous pour un premier rdv</h3>
     <p>
         Dans un premier temps il faut prendre un rendez-vous avec nous par mail ou par téléphone, afin de 
         fixez une date pour mettre en place un devis et nous indiqué quel genre de robot vous possedez ou compter acquérir.
     </p>
-    <p>Les information suivante seront demandé:</p>
+    <h4>Les information suivante seront demandé:</h4>
         <ul>
             <li>La marque de robot</li>
             <li>Un numéro de téléphone ou une adresse mail pour vous joindre</li>
             <li>votre adresse</li>
             <li>La taille du terrain peut être demander</li>
         </ul>
-   
+   <h3>2.Le devis sur mesure.</h3>
+   <p>
+    Nous privilégions les devis sur mesure pour vous evitez des coûts inutiles.
+    Dans un premiers temps nous allons mesurer les contours du terrain ainsi que les ilots à faire afin d'avoir une idée de la distance de fil à utiliser.
+    <br/>Nous voyons également avec vous les potentielle travaux à faire dans votre terrain certaine chose sont imcompatible avec la pose d'un robot.
+   </p>
+   <h4>Les choses qui impacte le prix du devis:</h4>
+   <ul>
+    <li>La longueur du fil a utiliser.</li>
+    <li>Le nombre d'ilots.</li>
+    <li>La complexité du terrain et donc le temps de travaille.</li>
+    <li>Dans certain cas le déplacement.</li>
+   </ul>
 </div>
 <?php include_once 'footer.php'?>
