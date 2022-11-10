0a3812e387477bbe4eb3e98d781d167427c1808d
Damien poix <39798430+DamienPoix@users.noreply.github.com>
2022-11-10 12:38:47 +0100

    hivernage(prix+infos supplementaire+ancre premium)

--- views/hivernageInfo.php
+++ views/hivernageInfo.php
@@ -5,7 +5,7 @@ include_once 'header.php';
 <div class="moderno">
     <h1 class="text-success text-center title">Hivernage et maintenance</h1>
     <h2>Nos différentes formules</h2>
-    <table class="table table-bordered text-center container ">
+    <table class="table table-bordered text-center container border-success">
         <thead class="moreDarker">
             <tr>
                 <th scope="col">Service inclus</th>
@@ -22,7 +22,7 @@ include_once 'header.php';
                 <th><i class="fa-solid fa-circle-check text-success "></i></th>
             </tr>
             <tr>
-                <th scope="row"><a href="#" class="noStyle">hivernage*</a></th>
+                <th scope="row">contrôle globale du robot</th>
                 <th><i class="fa-solid fa-circle-check text-success"></i></th>
                 <th><i class="fa-solid fa-circle-check text-success"></i></th>
                 <th><i class="fa-solid fa-circle-check text-success"></i></th>
@@ -34,32 +34,56 @@ include_once 'header.php';
                 <th><i class="fa-solid fa-circle-check text-success"></i></th>
             </tr>
             <tr>
+                <th scope="row">Affûtage jeu de lames</th>
+                <th><i class="fa-solid fa-circle-check text-success"></i></th>
+                <th><i class="fa-solid fa-circle-check text-success"></i></th>
+                <th><i class="fa-solid fa-circle-check text-success"></i></th>
+            </tr>
+            <tr>
                 <th scope="row">nettoyage simple</th>
                 <th><i class="fa-solid fa-circle-check text-success"></i></th>
                 <th><i class="fa-solid fa-circle-check text-success"></i></th>
                 <th><i class="fa-solid fa-circle-check text-success"></i></th>
             </tr>
             <tr>
-                <th scope="row"><a href="#" class="noStyle">nettoyage premium*</a></th>
+                <th scope="row">stockage Hivernal</th>
+                <th><i class="fa-solid fa-circle-xmark text-danger"></i></th>
+                <th><i class="fa-solid fa-circle-check text-success"></i></th>
+                <th><i class="fa-solid fa-circle-check text-success"></i></th>
+            </tr>
+            <tr>
+                <th scope="row">Entretient de la batterie</th>
+                <th><i class="fa-solid fa-circle-xmark text-danger"></i></th>
+                <th><i class="fa-solid fa-circle-check text-success"></i></th>
+                <th><i class="fa-solid fa-circle-check text-success"></i></th>
+            </tr>
+            <tr>
+                <th scope="row"><a href="#premium" class="noStyle">nettoyage premium*</a></th>
                 <th><i class="fa-solid fa-circle-xmark text-danger"></i></th>
                 <th><i class="fa-solid fa-circle-xmark text-danger"></i></th>
                 <th><i class="fa-solid fa-circle-check text-success"></i></th>
             </tr>
             <tr>
-                <th scope="row">remise en route au domicile</th>
+                <th scope="row">remise en route au printemps</th>
+                <th><i class="fa-solid fa-circle-xmark text-danger"></i></th>
                 <th><i class="fa-solid fa-circle-xmark text-danger"></i></th>
                 <th><i class="fa-solid fa-circle-check text-success"></i></th>
+            </tr>
+            <tr>
+                <th scope="row">enlèvement du robot au domicile</th>
+                <th><i class="fa-solid fa-circle-xmark text-danger"></i></th>
+                <th><i class="fa-solid fa-circle-xmark text-danger"></i></th>
                 <th><i class="fa-solid fa-circle-check text-success"></i></th>
             </tr>
             <tr>
                 <th scope="row bolder">Tarif 2022 (sais pas encore)</th>
-                <th>111€</th>
-                <th>111€</th>
-                <th>111€</th>
+                <th class="text-success">119€</th>
+                <th class="text-info">149€</th>
+                <th class="text-warning">199€</th>
             </tr>
         </tbody>
     </table>
-    <h3 class="title">Le nettoyage premium.</h3>
+    <h3 class="title" id="premium">Le nettoyage premium.</h3>
     <p>Chez <strong>Pose Robot PX</strong> nous proposons un service de nettoyage premium pour ce dernier nous utilisons un nettoyeur cryogéniques qui nous permet de nettoyer 
     vos robots en profondeur et sans risque pour les composants électronique</p>
     <p>Avec cette technologie nous pouvons avoir un effet neuf après le nettoyage nous sommes actuellement les seuls dans le secteur à proposé ce service</p>
